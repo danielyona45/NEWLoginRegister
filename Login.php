df75c4e4f1770c900eb81226d1a7aefa5ccae764
danielyona45 <34031253+danielyona45@users.noreply.github.com>
2017-11-27 15:38:26 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("my_host", "my_user", "my_password", "my_db");
     
     $username = $_POST["username"];
     $password = $_POST["password"];
